11f99e21f5d3586f7ee6ea58aba7d5770ca7ebd5
maryame <elkhalfimaryame@gmail.com>
2025-05-30 03:16:44 +0100

    n

--- src/main/java/com/easyapply/controller/AuthController.java
+++ src/main/java/com/easyapply/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,7 +79,7 @@ public class AuthController {
                 )
             ));
 
-            return ResponseEntity.ok(response);
+          
 
         } catch (Exception e) {
             return ResponseEntity.internalServerError()
